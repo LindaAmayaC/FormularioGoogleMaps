d5b85bd5554ffd8cd179ca89a49c981a4dd3de3f
lindaamaya <linda.amaya.cetina@gmail.com>
2022-03-10 13:42:22 -0500

    Agregando archivo index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UsandoGoogleMaps</title>
+</head>
+<body>
+<form id="formulario" method="POST" >
+                       
+                        <div class="tiempoEntrega">
+                            <label for="pwd">guardar ubicacion Actual</label>
+                            <input name="condGarantia" class="inputbtn" id="ubicacion" type="text" ></input>
+                        </div>
+                        <button type="button" class="btnEnviar" onclick="guardarInfo()"> Enviar</button> 
+                    </form>
+<script src="js.js"></script>
+</body>
+</html>
